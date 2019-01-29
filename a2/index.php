8e401c0377bbfc6657d761751ecbd3939d857d31
s3574546 <31581718+s3574546@users.noreply.github.com>
2019-01-29 23:34:55 +1030

    Update index.php

--- a2/index.php
+++ a2/index.php
@@ -7,41 +7,341 @@
     
     <!-- Keep wireframe.css for debugging, add your css to style.css -->
     <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
-    <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
+    <link id='stylecss' type="text/css" rel="stylesheet" href="style.css">
     <script src='../wireframe.js'></script>
   </head>
 
   <body>
 
     <header>
-      <div>Put company logo and name here</div>
-    </header>
-
+        
+        <img src='../media/LunardoCinema3.png'>
+        <h1>Lunardo Cinema</h1>
+      </header>
     <nav>
+        <ul>
+            <li><a href="#About">About Us</a></li>
+            <li><a href="#Seat">Seats and Prices</a></li>
+            <li><a href="#Now" class="active">Now Showing</a></li>
+            <li><a href="#Booking">Booking</a></li>
+        
+        
+        </ul>
+      </nav>
+      
+      <main>
 
-      <div>
-        <button type="button">About us</button>
-        <button type="button">Seats and Prices</button>
-        <button type="button">Now Showing</button>
-        <button type="button">Bookings</button>
-        </div>
+        <section class="sec2">
+            <p1> <a name="About"> About Us </a> </p1>
+            <h2>Lunardo Cinemas grand reopening</h2> 
 
-    </nav>
+            
+                <ul1>
+                <li>The Cinema has reopened after extensive improvements and renovation</li>
+                <li>There are new seats: standerd seats and reclinable first class seats</li>
+                <li>The projection and sound systems are upgraded with 3D Dolby Vision projection and Dolby Atmos sound</li>
+                </ul1>
+            
+            
 
-    <main>
-      <article id='Website Under Construction'>
-    <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
-        <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
-      </article>
+      </section>
+      <section class="sec2">
+    
+
+  
+
+    <br>
+    <br>
+    <p1> <a name="Seat"> Seat and Prices </a></p1>
+    <br>
+    <br>
+    <table>
+          <tr>
+              <th>Seat Type</th>
+              <th>Seat Code</th>
+              <th>All day Monday and Wednesday AND 12pm on Weekdays</th>
+              <th>All other times</th>
+            </tr>
+          <tr>
+                <td>Stadard Adult</td>
+                <td>STA</td>
+                <td>14.00</td>
+                <td>19.80</td>
+        </tr>
+        <tr>
+                <td>Stadard Concession</td>
+                <td>STP</td>
+                <td>12.50</td>
+                <td>17.50</td>
+        </tr>
+        <tr>
+                <td>Stadard Child</td>
+                <td>STC</td>
+                <td>11.00</td>
+                <td>15.30</td>
+        </tr>        
+        <tr>
+                <td>First Class Adult</td>
+                <td>FCA</td>
+                <td>24.00</td>
+                <td>30.00</td>
+        </tr>        
+         <tr>
+                <td>First Class Concession</td>
+                <td>FCP</td>
+                <td>22.50</td>
+                <td>27.00</td>
+        </tr>
+        <tr>
+                <td>First Class Child</td>
+                <td>FCC</td>
+                <td>21.00</td>
+                <td>24.00</td>
+        </tr>
+    </table>
+          </section>
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>  
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <section class="sec2">
+    <p1> <a name="Now"> Now Showing </a> </p1>
+        <br>
+        
+        <body1>
+            
+            <div style="display: inline-block; width: 360px;"> 
+            <div style=" background: -webkit-linear-gradient(top, #3c3c3c 0, #222222 100%);">
+               
+                <img src='../spidersweb.jpg'>
+                <h3 style=" text-align: center;">The Girl in the Spider's Web (G)</h3>
+            
+                <ul style="background: -webkit-linear-gradient(top, #3c3c3c 0, #222222 100%); text-align: center;">
+                    <li> Mon - 6pm(18:00)</li>
+                    <li> Tue - 6pm(18:00)</li>
+                    <li> Wed - 6pm(18:00)</li>
+                    <li> Thu - 10pm(22:00)</li>
+                    <li> Fri - 9pm(21:00)</li>
+                    <li> Sat - 10pm(22:00)</li>
+                    <li> Sun - 8pm(22:00)</li>
+                </ul>
+               </div>
+            </div>
+                <div style="display: inline-block; width: 350px;"> 
+                <div style=" background: -webkit-linear-gradient(top, #3c3c3c 0, #222222 100%);">
+                <img src="A-Star-Is-Born-poster-2.jpg">
+                <h3 style=" text-align: center;">A Star is Born (MA)</h3>
+            
+                
+            
+                <ul style="background: -webkit-linear-gradient(top, #3c3c3c 0, #222222 100%); text-align: center;">
+                    <li>-</li>
+                    <li>-</li>
+                    <li> Wed - 8pm(20:00)</li>
+                    <li> Thu - 8pm(20:00)</li>
+                    <li> Fri - 11pm(23:00)</li>
+                    <li> Sat - 8pm(20:00)</li>
+                    <li> Sun - 6pm(18:00)</li>
+                </ul>
+                </div>
+            </div>
+                <div style="display: inline-block; width: 350px;"> 
+                <div style=" background: -webkit-linear-gradient(top, #3c3c3c 0, #222222 100%);">
+                <img src="ralph.jpg">
+                <h3 style=" text-align: center;">Ralph Breaks the Internet (R)</h3>
+            
+                <ul style="background: -webkit-linear-gradient(top, #3c3c3c 0, #222222 100%); text-align: center;">
+                    <li> Mon - 6pm(18:00)</li>
+                    <li> Tue - 6pm(18:00)</li>
+                    <li> Wed - 6pm(18:00)</li>
+                    <li> - </li>
+                    <li> Fri - 9pm(21:00)</li>
+                    <li> Sat - 10pm(22:00)</li>
+                    <li> Sun - 8pm(22:00)</li>
+                </ul>
+               </div>
+            </div>
+                    
+                <div style="display: inline-block; width: 350px;"> 
+                <div style=" background: -webkit-linear-gradient(top, #3c3c3c 0, #222222 100%);">
+                <img src="boy.jpeg">
+                <h3 style=" text-align: center;">Boy Erased (R)</h3>
+            
+                <ul style="background: -webkit-linear-gradient(top, #3c3c3c 0, #222222 100%); text-align: center;">
+                    <li>-</li>
+                    <li>-</li>
+                    <li>-</li>
+                    <li> Wed - 6pm(18:00)</li>
+                    <li> Fri - 9pm(21:00)</li>
+                    <li> Sat - 10pm(22:00)</li>
+                    <li> Sun - 8pm(22:00)</li>
+                </ul>
+               </div>
+         </div>
+        
+    </section>
+        
+
+        
+          </section>
+        
+        
+    <br>    
+        
+    <br>
+            <section>
+    <table>
+          <tr>
+              <th>Movie Title</th>
+              <th>#Sess</th>
+              <th>Movie Id</th>
+              <th>Mon - Tue</th>
+              <th>Wed - Fri</th>
+              <th>Sat - Sun</th>
+            </tr>
+          <tr>
+                <td>The Girl in the Spider's Web</td>
+                <td>5</td>
+                <td>ACT</td>
+                <td> - </td>
+                <td>9pm(21:00)</td>
+                <td>6pm(18:00)</td>
+        </tr>
+        <tr>
+                <td>A Star is Born</td>
+                <td>4</td>
+                <td>RMC</td>
+                <td>6pm(18:00)</td>
+                <td> - </td>
+                <td>3pm(15:00)</td>
+        </tr>
+        <tr>
+                <td>Ralph Breaks the Internet</td>
+                <td>7</td>
+                <td>ANM</td>
+                <td>12pm(12:00)</td>
+                <td>6pm(18:00)</td>
+                <td>12pm(12:00)</td>
+        </tr>        
+        <tr>
+                <td>Boy Erased</td>
+                <td>5</td>
+                <td>AHF</td>
+                <td> - </td>
+                <td>12pm(12:00)</td>
+                <td>9pm(21:00)</td>
+        </tr>        
+
+    </table>
+          </section>
+          
+<body3>
+          <div class="wrapper">
+              <div class="box box1"><h2>A Girl in Spider's Web</h2>
+              <h3>Plot description</h3>
+              <p>Fired from the National Security Agency, Frans Balder recruits hacker Lisbeth Salander to steal FireWall, a computer program that can access codes for nuclear weapons worldwide. The download soon draws attention from an NSA agent who traces the activity to Stockholm. Further problems arise when Russian thugs take Lisbeth's laptop and kidnap a math whiz who can make FireWall work. Now, Lisbeth and an unlikely ally must race against time to save the boy and recover the codes to avert disaster.</p>
+              </div>
+              
+              
+              
+              <div class="box box2"><iframe width="560" height="315" src="https://www.youtube.com/embed/XKMSP9OKspQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+              
+              
+              <div class="box box3"><h2>Make a Booking:</h2>
+              <div>
+              <table>
+          <tr>
+              <th>Mon - 6pm(18:00)</th>
+              <th>Tue - 6pm(18:00)</th>
+              <th>Wed - 6pm(18:00)</th>
+              <th>Thu - 10pm(22:00)</th>
+              <th>Fri - 9pm(21:00)</th>
+              
+         </tr>
+            <tr>
+                <td>Sat - 10pm(22:00)</td>
+                <td>Sun - 8pm(22:00)</td>              
+                  </tr>
+                  
+                  </table>
+              </div>
+              
+              
+              </div>
+    </div>
+          
+
+ </body3>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>  
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+          <section class="sec2">
+    <p1> <a name="Booking"> Booking </a> </p1>
+          </section>
+    </body>
+
+
+      
+      
+      
+      
+    
+        
+        
+     
     </main>
 
-    <footer>
-      <div>&copy;<script>
-        document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
-      <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
-      <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
-    </footer>
 
-  </body>
+ 
 </html>
